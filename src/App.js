db8fa8f339fae0570729892cda2de098c214cc80
Anupam kumar <anupamit011@gmail.com>
2022-01-14 21:26:42 +0530

    updated with basic home and widgets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import './App.css';
-import Sidebar from './Sidebar';
+import Sidebar from './Sidebar'
+import Feed from './Feed';
+import Widgets from './Widgets';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       {/* <h1>Shail</h1> */}
       <Sidebar/>
+      <Feed/>
+      <Widgets/>
     </div>
   );
 }
