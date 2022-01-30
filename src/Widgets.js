e9bae9362c50f8b80d0f520cf07bc30d38628c5c
Anupam kumar <anupamit011@gmail.com>
2022-01-30 14:26:31 +0530

    widgets modify

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';import React from 'react';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 import "./Widgets.css"
 
 function Widgets() {
     return (
         <div className='widgets'>
-            <h2>Widgets</h2>
+            <div className='widgets__input'>
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder='Search Twitter' type="text" />
+            </div>
+            <div className='widgets__widgetContainer'>
+            <h2>What's happening</h2>
+
+            <TwitterTweetEmbed tweetId={"858551177860055040"} />
+            </div>
         </div>
     );
 }
